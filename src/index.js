2f75961c6e06d215554db2e200fdc76bd43f0679
massaracsh7 <fatum7@gmail.com>
2024-08-14 15:35:41 +0300

    feat: add api fetch

--- src/index.js
+++ src/index.js
@@ -12,3 +12,43 @@ const changeActiveBtn = (event) => {
 buttons.forEach((button) => {
   button.addEventListener("click", changeActiveBtn);
 });
+
+const API_URL = 'https://pet-planet-api.onrender.com';
+const productList = document.querySelector(".store__list");
+
+const createProductCard = (product) => {
+  const productCard = document.createElement('li');
+  productCard.classList.add('store__item');
+  productCard.innerHTML = `<article class="store__product product">
+                <img src="${API_URL}${product.photoUrl}" alt=${product.name} class="product__img" width="388" height="261">
+                <h3 class="product__title">${product.name}</h3>
+                <p class="product__price">${product.price}&nbsp;₽</p>
+                <button class="product__btn-add">Заказать</button>
+              </article>`;
+  return productCard;
+};
+
+const renderProducts = (products) => {
+  productList.textContent = '';
+  products.forEach((product) => {
+    const productCard = createProductCard(product);
+    productList.append(productCard);
+  })
+}
+
+const fetchProductByCategory = async (category) => {
+  try {
+    const response = await fetch(`${API_URL}/api/products/category/${category}`);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const products = await response.json();
+
+    renderProducts(products);
+  }
+  catch (error) {
+    console.error(`Ошибка запроса товаров ${error}`);
+  }
+}
+
+fetchProductByCategory('Домики');
