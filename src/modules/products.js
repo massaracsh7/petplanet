d061cb3f2a615bdd80e80ae7d89f1f05b9ca3e65
massaracsh7 <fatum7@gmail.com>
2024-09-16 10:00:53 +0300

    refactor: updates styles

--- src/modules/products.js
+++ src/modules/products.js
@@ -3,20 +3,30 @@ import { productList } from './selectors.js';
 
 export const fetchProductByCategory = async (category) => {
   try {
+    const loader = document.createElement('div');
+    loader.classList.add('loader');
+    loader.textContent = 'Загрузка товаров...';
+
+    productList.textContent = '';
+    productList.appendChild(loader);
+
     const response = await fetch(`${API_URL}/api/products/category/${category}`);
-    console.log(response);
     if (!response.ok) {
       throw new Error(response.status);
     }
+
     const products = await response.json();
     renderProducts(products);
   } catch (error) {
     console.error(`Ошибка запроса товаров ${error}`);
+  } finally {
+    const loader = document.querySelector('.loader');
+    if (loader) loader.remove();
   }
 };
 
 export const renderProducts = (products) => {
-  productList.textContent = '';
+  productList.textContent = ''; 
   products.forEach((product) => {
     const productCard = createProductCard(product);
     productList.append(productCard);
@@ -28,7 +38,7 @@ export const createProductCard = (product) => {
   productCard.classList.add('store__item');
   productCard.innerHTML = `
     <article class="store__product product">
-      <img src="${API_URL}${product.photoUrl}" alt=${product.name} class="product__img" width="388" height="261">
+      <img src="${API_URL}${product.photoUrl}" alt="${product.name}" class="product__img" width="388" height="261">
       <h3 class="product__title">${product.name}</h3>
       <p class="product__price">${product.price}&nbsp;₽</p>
       <button class="product__btn-add" data-id="${product.id}">Заказать</button>
